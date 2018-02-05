64898dee2c4292870920c8e22095012fad18cc12
AdamPI314 <shirong.bai@colorado.edu>
2018-02-05 15:32:43 -0700

    minor

--- main_driver.py
+++ main_driver.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     TIME_I = time.time()
 
     FILE_DIR = os.path.abspath(os.path.join(os.path.realpath(
-        sys.argv[0]), os.pardir, os.pardir, os.pardir))
+        sys.argv[0]), os.pardir, os.pardir, os.pardir, os.pardir, "SOHR_DATA"))
     print(FILE_DIR)
 
     S_A_S = global_settings.get_s_a_setting(FILE_DIR)
